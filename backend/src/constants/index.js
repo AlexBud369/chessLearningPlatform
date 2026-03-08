eab96b71945582f80ed25b4036897f1cbdf76f2c
AlexBud369 <143994731+AlexBud369@users.noreply.github.com>
2026-03-08 20:28:03 +0300

    feat: add token and cookie constants

--- backend/src/constants/index.js
+++ backend/src/constants/index.js
@@ -4,5 +4,14 @@ module.exports = {
         EMAIL_MAX_LENGTH: 255,
         PASSWORD_HASH_LENGTH: 100,
         AVATAR_MAX_LENGTH: 255,
+    },
+     TOKEN: {
+        REFRESH_EXPIRES_IN_MS: 7 * 24 * 60 * 60 * 1000,  
+    },
+    COOKIE: {
+        REFRESH_TOKEN_NAME: 'refreshToken',
+        MAX_AGE: 7 * 24 * 60 * 60 * 1000,
+        HTTP_ONLY: true,
+        SAME_SITE: 'strict',
     }
 };
