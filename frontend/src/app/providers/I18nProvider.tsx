6a5d230b4b27666919738f1cb53cfc8184d0667d
AlexBud369 <143994731+AlexBud369@users.noreply.github.com>
2026-04-12 20:01:32 +0300

    feat: add dependencies and change import

--- frontend/src/app/providers/I18nProvider.tsx
+++ frontend/src/app/providers/I18nProvider.tsx
@@ -1,6 +1,6 @@
 import React, { Suspense } from 'react';
 import { I18nextProvider } from 'react-i18next';
-import i18n from '../../shared/i18n';
+import i18n from '../../shared/i18n/i18nInit';
 
 export const I18nProvider = ({ children }: { children: React.ReactNode }) => {
   return (
