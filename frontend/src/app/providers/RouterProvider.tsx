a025bd2b0031d8ae3cc6bb90463f490a7127c42b
AlexBud369 <143994731+AlexBud369@users.noreply.github.com>
2026-04-12 19:57:41 +0300

    feat: add RouterProvider with React Router v6 routes

---
+++ frontend/src/app/providers/RouterProvider.tsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HomePage } from '../../pages/home/HomePage';
+import { LoginPage } from '../../pages/auth/login/LoginPage';
+import { RegisterPage } from '../../pages/auth/register/RegisterPage';
+import { ProfilePage } from '../../pages/profile/ProfilePage';
+
+export const RouterProvider = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+      <Route path="/profile" element={<ProfilePage />} />
+    </Routes>
+  </BrowserRouter>
+);
