029e27d82f966bd2726b53b64ca413ad0573f8af
AlexBud369 <143994731+AlexBud369@users.noreply.github.com>
2026-05-17 15:30:27 +0300

    feat: add favorite slice and add it it store

--- frontend/src/app/store.ts
+++ frontend/src/app/store.ts
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import userReducer from '../entities/user/model/store';
 import themeReducer from '../entities/theme/model/store';
 import courseFiltersReducer from '../entities/course/model/store';
+import favoritesReducer from '../entities/favorite/model/favoritesSlice';
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
     theme: themeReducer,
     courses: courseFiltersReducer,
+    favorites: favoritesReducer,
   },
 });
 
