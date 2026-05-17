e12a7d363f355fce2a8e0d789e4d2dda3ca7760a
AlexBud369 <143994731+AlexBud369@users.noreply.github.com>
2026-05-17 12:55:04 +0300

    feat: add routes and tostify container

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -3,15 +3,18 @@ import { ThemeProvider } from './providers/ThemeProvider';
 import { RouterProvider } from './providers/RouterProvider';
 import { SessionInitializer } from './providers/SessionInitializer';
 import { I18nProvider } from './providers/I18nProvider';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
-     <StoreProvider>
+    <StoreProvider>
       <ThemeProvider>
         <I18nProvider>
           <SessionInitializer>
-          <RouterProvider />
-        </SessionInitializer>
+            <RouterProvider />
+            <ToastContainer position="bottom-right" autoClose={3000} />
+          </SessionInitializer>
         </I18nProvider>
       </ThemeProvider>
     </StoreProvider>
