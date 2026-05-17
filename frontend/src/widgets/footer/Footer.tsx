9a7662fa77d8d3168f9f379e23b76714751a174c
AlexBud369 <143994731+AlexBud369@users.noreply.github.com>
2026-05-17 12:55:21 +0300

    feat: add footer and locales

---
+++ frontend/src/widgets/footer/Footer.tsx
@@ -0,0 +1,37 @@
+import { Box, Container, Typography, Link } from '@mui/material';
+import { useTranslation } from 'react-i18next';
+
+export const Footer = () => {
+  const { t } = useTranslation();
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <Box
+      component="footer"
+      sx={{
+        py: 3,
+        px: 2,
+        mt: 'auto',
+        backgroundColor: (theme) =>
+          theme.palette.mode === 'light'
+            ? theme.palette.grey[200]
+            : theme.palette.grey[800],
+      }}
+    >
+      <Container maxWidth="lg">
+        <Typography variant="body2" color="text.secondary" align="center">
+          {t('footer.copyright', { year: currentYear })}
+        </Typography>
+        <Typography variant="body2" color="text.secondary" align="center" sx={{ mt: 1 }}>
+          <Link href="/" color="inherit">
+            {t('footer.about')}
+          </Link>{' '}
+          |{' '}
+          <Link href="/" color="inherit">
+            {t('footer.contact')}
+          </Link>
+        </Typography>
+      </Container>
+    </Box>
+  );
+};
