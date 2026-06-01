739a8359ef1a549a7f43225486ba5d1d710b0433
AlexBud369 <143994731+AlexBud369@users.noreply.github.com>
2026-06-01 11:32:20 +0300

    refactor: remove useless files

--- frontend/src/shared/lib/PrivateRoute.tsx
+++
@@ -1,7 +0,0 @@
-import { Navigate, Outlet } from 'react-router-dom';
-import { useAppSelector } from './hooks';
-
-export const PrivateRoute = () => {
-  const user = useAppSelector(state => state.user.user);
-  return user ? <Outlet /> : <Navigate to="/login" replace />;
-};
